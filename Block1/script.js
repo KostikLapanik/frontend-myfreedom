7ff3c946fc3f47a578bcbc1d2a0ff45465870f21
Kostik Lapanik <lapanik.kostik@gmail.com>
2024-10-19 17:39:45 +0300

    Second task

--- Block1/script.js
+++ Block1/script.js
@@ -1,7 +1,20 @@
+// 1
 let userNumber = prompt('Введите число');
 
 if (userNumber % 2 == 0){
     alert('Чётное!')
 } else {
     alert('Не чётное!')
+}
+
+// 2
+let firstNumber = prompt('Введите первое число')
+let secondNumber = prompt('Введите второе число')
+
+if (firstNumber == secondNumber){
+    alert('Числа равны.')
+} else if (firstNumber > secondNumber){
+    alert('Первое число больше!')
+} else{
+    alert('Второе число больше первого!')
 }
