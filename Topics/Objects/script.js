e5e3822b3acf7f5c22df3dda55ea18ac43800548
Kostik Lapanik <lapanik.kostik@gmail.com>
2024-11-02 17:54:16 +0300

    new topic

--- Topics/Objects/script.js
+++ Topics/Objects/script.js
@@ -57,3 +57,67 @@ function multiplyNumeric(obj) {
 
 const newMenu = multiplyNumeric(menu);
 console.log(newMenu);
+
+
+// 2 урок
+
+
+// 1
+const original = {
+    a: 1,
+    b: {
+        c: 3
+    }
+}
+
+function shallowClone(obj) {
+    let newObj = {};
+
+    for (let key in obj) {
+        newObj[key] = obj[key]
+    }
+
+    return newObj
+}
+
+const clone = shallowClone(original)
+console.log(clone);
+clone.b.c = 3;
+console.log(original);
+
+// 2
+rectangle = {
+    width: 100,
+    height: 200,
+    getArea() {
+        return this.width * this.height
+    },
+    getPerimeter() {
+        return 2 * (this.width + this.height)
+    },
+}
+
+console.log(rectangle.getArea())
+console.log(rectangle.getPerimeter())
+
+// 3
+account = {
+    balance: 0,
+    desposit(amount) {
+        this.balance += amount
+    },
+    withdraw(amount){
+        if (this.balance > amount){
+            this.balance -= amount
+        } else {
+            alert('Недостаточно средст')
+        }
+    },
+    getBalance(){
+        alert(this.balance)
+    },
+}
+
+console.log(account.desposit(100))
+console.log(account.withdraw(200))
+console.log(account.getBalance())
