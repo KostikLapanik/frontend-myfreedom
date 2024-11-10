637607c6d85a6d9be31a1eb0adfc6f8ce9c26b7b
Kostik Lapanik <lapanik.kostik@gmail.com>
2024-11-10 13:38:56 +0300

    new topic

---
+++ Topics/Array/script.js
@@ -0,0 +1,27 @@
+let array = [];
+
+// 1
+let fiveNumbersArray = [1, 2, 3, 4, 5]
+
+// 2
+let fruits = ['apple', 'banana', 'orange']
+
+// 3
+let names = ['John', 'Alice', 'Bob']
+
+// 4
+let arrayOf = Array.of('hello', 'world', 'javascript')
+
+// 5
+let numbers = [1, 2, 3, 4, 5]
+console.log(numbers[2]);
+
+// 6
+numbers[0]
+numbers.at(1)
+numbers.shift()
+
+// 7
+numbers[4]
+numbers.at(-1)
+numbers.pop()
