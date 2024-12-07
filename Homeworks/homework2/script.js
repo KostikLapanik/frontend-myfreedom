23b28c9e9731ba07d66ffa80a7a2909b9c511182
Kostik Lapanik <lapanik.kostik@gmail.com>
2024-12-07 15:36:53 +0300

    fix some mistakes

---
+++ Homeworks/homework2/script.js
@@ -0,0 +1,19 @@
+// 1
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+alert(Number(a) + Number(b));
+
+// 2ss
+
+let str = "16";
+let num = 4;
+let result = Number(str) + num;
+console.log(result);
+
+// 3адача повышенной сложности
+
+// const valuePI = "3.14";
+// const bool = true;
+// let result = String(Math.round(Number(valuePI) * 2 + Number(bool)));
+// console.log(result);
---
+++ Homeworks/homework3/script.js
@@ -0,0 +1,48 @@
+// 1
+
+let JavaScriptOficialName = prompt('Какое «официальное» название JavaScript?')
+
+if (JavaScriptOficialName == 'ECMAScript'){
+    alert('Верно!')
+} else{
+    alert('Не знаете? ECMAScript!')
+}
+s
+
+let login = prompt('Введите логин:');
+let message;
+
+message = (login === 'Сотрудник') 
+
+  ? 'Привет' 
+  : (login === 'Директор') 
+  ? 'Здравствуйте' 
+  : (login === '') 
+  ? 'Нет логина' 
+  : '';
+
+alert(message);
+console.log(message);
+
+// 3
+
+let userLogin = prompt('Введите логин:');
+let result;
+
+if (userLogin == 'Админ'){
+    let password = prompt('Введите пароль:');
+
+    if (password == 'Я главный'){
+        result = 'Здравствуйте!';
+    } else if (password == null || password == '') {
+        result = 'Отменено';
+    }
+
+} else if(userLogin == '' || userLogin == null){
+    result = 'Отменено';
+} else{
+    result = 'Я вас не знаю';
+}
+
+alert(result);
+console.log(result);
